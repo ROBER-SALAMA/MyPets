551ffbff7e4bd0d34aaa09e6a5ad1778d435b158
Roberto Salama <102178835+ROBER-SALAMA@users.noreply.github.com>
2023-10-30 23:43:22 -0600

    cambios

--- MyPetsWeb.UIAppWebAspCore/Startup.cs
+++ MyPetsWeb.UIAppWebAspCore/Startup.cs
@@ -17,7 +17,8 @@ namespace SeguridadWeb.UI.AppWebAspCore
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public
+        Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
